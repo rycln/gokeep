637cf7a419055f79bf833b80d6b3c27d2d185b7f
Khalikov Ruslan <rycln1@gmail.com>
2025-07-22 20:59:25 +0300

    feat: added item storage

---
+++ internal/client/storage/itemstorage.go
@@ -0,0 +1,87 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/rycln/gokeep/internal/shared/models"
+)
+
+type ItemStorage struct {
+	db *sql.DB
+}
+
+func NewItemStorage(db *sql.DB) *ItemStorage {
+	return &ItemStorage{
+		db: db,
+	}
+}
+
+func (s *ItemStorage) AddItem(ctx context.Context, info *models.ItemInfo, content []byte) error {
+	_, err := s.db.ExecContext(
+		ctx,
+		sqlAddItem,
+		info.ID,
+		info.UserID,
+		info.ItemType,
+		info.Name,
+		content,
+		info.Metadata,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *ItemStorage) GetUserItemsInfo(ctx context.Context, uid models.UserID) (itemsinfo []models.ItemInfo, err error) {
+	rows, err := s.db.QueryContext(ctx, sqlGetUserItemsInfo, uid)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if rowsCloseErr := rows.Close(); rowsCloseErr != nil {
+			err = fmt.Errorf("%v; rows close failed: %w", err, rowsCloseErr)
+		}
+	}()
+
+	for rows.Next() {
+		var info models.ItemInfo
+
+		err = rows.Scan(
+			&info.ID,
+			&info.UserID,
+			&info.ItemType,
+			&info.Name,
+			&info.Metadata,
+			&info.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		itemsinfo = append(itemsinfo, info)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return itemsinfo, nil
+}
+
+func (s *ItemStorage) GetContentByName(ctx context.Context, name string) ([]byte, error) {
+	row := s.db.QueryRowContext(ctx, sqlGetItemByName, name)
+
+	var content []byte
+
+	err := row.Scan(&content)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
---
+++ internal/client/storage/queries.go
@@ -0,0 +1,37 @@
+package storage
+
+const sqlCreateItemsTable = `
+	CREATE TABLE IF NOT EXISTS items (
+		id TEXT PRIMARY KEY,
+		user_id TEXT NOT NULL,
+		type TEXT NOT NULL,
+		name TEXT NOT NULL UNIQUE,
+		encrypt_content BLOB NOT NULL,
+		metadata TEXT,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)
+`
+const sqlAddItem = `
+	INSERT INTO items
+	(id, user_id, type, name, enc_content, metadata) 
+	VALUES ($1, $2, $3, $4, $5, $6)
+`
+
+const sqlGetItemByName = `
+	SELECT 
+		encrypt_content,
+	FROM items
+	WHERE name = $1
+`
+
+const sqlGetUserItemsInfo = `
+	SELECT 
+		id,
+		user_id,
+		type,
+		name, 
+		metadata,
+		updated_at 
+	FROM items
+	WHERE user_id = $1
+`
