704aac15911d6b9d0e99fa5f8b7dca660304bf74
Khalikov Ruslan <rycln1@gmail.com>
2025-07-24 21:56:00 +0300

    feat: updated main.go

--- cmd/gophkeeper/client/main.go
+++ cmd/gophkeeper/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"log"
@@ -9,10 +10,16 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	client "github.com/rycln/gokeep/internal/client/grpc"
 	"github.com/rycln/gokeep/internal/client/services"
+	"github.com/rycln/gokeep/internal/client/storage"
 	"github.com/rycln/gokeep/internal/client/tui"
 	"github.com/rycln/gokeep/internal/client/tui/auth"
+	"github.com/rycln/gokeep/internal/client/tui/vault"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+)
+
+const (
+	DBpath = "./gophkeeper.db"
 )
 
 func main() {
@@ -29,11 +36,21 @@ func main() {
 	}
 	defer conn.Close()
 
+	db, err := storage.NewDB(DBpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	itemStorage := storage.NewItemStorage(db)
+
 	authService := services.NewAuthService(client.NewUserClient(conn))
+	itemService := services.NewItemService(itemStorage)
 
 	authScreen := auth.InitialModel(authService)
+	vaultScreen := vault.NewModel(itemService, context.Background())
 
-	p := tea.NewProgram(tui.InitialRootModel(authScreen))
+	p := tea.NewProgram(tui.InitialRootModel(authScreen, vaultScreen))
 	if _, err := p.Run(); err != nil {
 		os.Exit(1)
 	}
