776a681d60e5faad24164143d37ed7795d5cdb40
Khalikov Ruslan <rycln1@gmail.com>
2025-07-30 09:22:12 +0300

    feat: added client app layer

--- client/cmd/gophkeeper/main.go
+++ client/cmd/gophkeeper/main.go
@@ -1,71 +1,19 @@
 package main
 
 import (
-	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"log"
-	"os"
-	"time"
 
-	tea "github.com/charmbracelet/bubbletea"
-	client "github.com/rycln/gokeep/client/internal/grpc"
-	"github.com/rycln/gokeep/client/internal/services"
-	"github.com/rycln/gokeep/client/internal/storage"
-	"github.com/rycln/gokeep/client/internal/tui"
-	"github.com/rycln/gokeep/client/internal/tui/screens/add"
-	"github.com/rycln/gokeep/client/internal/tui/screens/auth"
-	"github.com/rycln/gokeep/client/internal/tui/screens/update"
-	"github.com/rycln/gokeep/client/internal/tui/screens/vault"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-)
-
-const (
-	DBpath  = "./gophkeeper.db"
-	timeout = time.Duration(5) * time.Second
+	"github.com/rycln/gokeep/client/internal/app"
 )
 
 func main() {
-	//временно в main
-	certPool, _ := x509.SystemCertPool()
-
-	tlsConfig := &tls.Config{
-		RootCAs:    certPool,
-		MinVersion: tls.VersionTLS12,
-	}
-	conn, err := grpc.NewClient(":50051", grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	db, err := storage.NewDB(DBpath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	err = storage.InitDB(ctx, db)
+	app, err := app.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	itemStorage := storage.NewItemStorage(db)
-
-	authService := services.NewAuthService(client.NewUserClient(conn))
-	itemService := services.NewItemService(itemStorage)
-
-	authScreen := auth.InitialModel(authService, timeout)
-	vaultScreen := vault.InitialModel(itemService, timeout)
-	addScreen := add.InitialModel(itemService, timeout)
-	updateScreen := update.InitialModel(itemService, timeout)
-
-	p := tea.NewProgram(tui.InitialRootModel(authScreen, vaultScreen, addScreen, updateScreen))
-	if _, err := p.Run(); err != nil {
-		os.Exit(1)
+	err = app.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
---
+++ client/internal/app/app.go
@@ -0,0 +1,152 @@
+package app
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	client "github.com/rycln/gokeep/client/internal/grpc"
+	"github.com/rycln/gokeep/client/internal/services"
+	"github.com/rycln/gokeep/client/internal/storage"
+	"github.com/rycln/gokeep/client/internal/tui"
+	"github.com/rycln/gokeep/client/internal/tui/screens/add"
+	"github.com/rycln/gokeep/client/internal/tui/screens/auth"
+	"github.com/rycln/gokeep/client/internal/tui/screens/update"
+	"github.com/rycln/gokeep/client/internal/tui/screens/vault"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+const (
+	grpcTarget = ":50051"
+	DBpath     = "./gophkeeper.db"
+	timeout    = time.Duration(5) * time.Second
+)
+
+type App struct {
+	tui  *tea.Program
+	conn *grpc.ClientConn
+	db   *sql.DB
+}
+
+func New() (*App, error) {
+	certPool, _ := x509.SystemCertPool()
+
+	tlsConfig := &tls.Config{
+		RootCAs:    certPool,
+		MinVersion: tls.VersionTLS12,
+	}
+	conn, err := grpc.NewClient(grpcTarget, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	if err != nil {
+		return nil, fmt.Errorf("grpc client error: %v", err)
+	}
+
+	db, err := storage.NewDB(DBpath)
+	if err != nil {
+		return nil, fmt.Errorf("db creation error: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	err = storage.InitDB(ctx, db)
+	if err != nil {
+		return nil, fmt.Errorf("db init error: %v", err)
+	}
+
+	itemStorage := storage.NewItemStorage(db)
+
+	authService := services.NewAuthService(client.NewUserClient(conn))
+	itemService := services.NewItemService(itemStorage)
+
+	authScreen := auth.InitialModel(authService, timeout)
+	vaultScreen := vault.InitialModel(itemService, timeout)
+	addScreen := add.InitialModel(itemService, timeout)
+	updateScreen := update.InitialModel(itemService, timeout)
+
+	p := tea.NewProgram(tui.InitialRootModel(authScreen, vaultScreen, addScreen, updateScreen))
+
+	return &App{
+		tui:  p,
+		conn: conn,
+		db:   db,
+	}, nil
+}
+
+func (app *App) Run() error {
+	go func() {
+		if _, err := app.tui.Run(); err != nil {
+			os.Exit(1)
+		}
+	}()
+
+	printBuildInfo()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	<-shutdown
+
+	err := app.shutdown()
+	if err != nil {
+		return fmt.Errorf("shutdown error: %v", err)
+	}
+
+	err = app.cleanup()
+	if err != nil {
+		return fmt.Errorf("cleanup error: %v", err)
+	}
+
+	log.Println(strings.TrimPrefix(os.Args[0], "./") + " shutted down gracefully")
+
+	return nil
+}
+
+func (app *App) shutdown() error {
+	app.tui.Quit()
+
+	return nil
+}
+
+func (app *App) cleanup() error {
+	if err := app.conn.Close(); err != nil {
+		return fmt.Errorf("conn close failed: %w", err)
+	}
+
+	if err := app.db.Close(); err != nil {
+		return fmt.Errorf("storage close failed: %w", err)
+	}
+
+	return nil
+}
+
+func printBuildInfo() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
